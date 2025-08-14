551e02786bd4fc8abdcad927424b49c52b42a230
riya <riyaaggarwal2206@gmail.com>
2025-08-14 10:51:21 +0530

    third commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './configs/mongodb.js';
+import { clerkWebHooks } from './controllers/webhooks.js';
+await connectDB()
+
+const app=express()
+app.use(express.json())
+app.use(cors())
+app.get('/',(req,res)=> res.send("api working"))
+app.post('/clerk',clerkWebHooks)
+const port=process.env.PORT || 5000
+app.listen(port,()=>{
+    console.log(`server running on port ${port}`)
+})
