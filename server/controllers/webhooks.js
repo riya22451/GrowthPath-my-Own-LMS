e20d1a75866c8f34a71417bd5e9c437b3a5e6c75
riya <riyaaggarwal2206@gmail.com>
2025-08-14 11:12:38 +0530

    corrected mistake

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -13,7 +13,7 @@ export const clerkWebHooks=async(req,res)=>{
         case 'user.created':{
             const userData={
                 _id:data.id,
-                email:data.email_address[0].email_address,
+                email:data.email_addresses[0].email_address,
                 name:data.first_name+" "+data.last_name,
                 imageUrl:data.iamge_url,
             }
@@ -24,7 +24,7 @@ export const clerkWebHooks=async(req,res)=>{
         case 'user.updated':{
              const userData={
                 
-                email:data.email_address[0].email_address,
+                email:data.email_addresses[0].email_address,
                 name:data.first_name+" "+data.last_name,
                 imageUrl:data.iamge_url,
             }
