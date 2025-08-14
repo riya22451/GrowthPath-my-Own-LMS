d3c36a0927901af10d34d8e45c74fd6311469774
riya <riyaaggarwal2206@gmail.com>
2025-08-14 11:23:57 +0530

    added express.raw

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app=express()
 app.use(express.json())
 app.use(cors())
 app.get('/',(req,res)=> res.send("api working"))
-app.post('/clerk',clerkWebHooks)
+app.post('/clerk', express.raw({ type: "application/json" }),clerkWebHooks)
 const port=process.env.PORT || 5000
 app.listen(port,()=>{
     console.log(`server running on port ${port}`)
