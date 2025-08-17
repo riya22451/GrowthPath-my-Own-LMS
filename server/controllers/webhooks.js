4e5a746ca4695ba98cf5fb4e746d0b611527b00d
riya22451 <riyaaggarwal226@gmail.com>
2025-08-17 10:36:24 +0530

    Update webhooks.js mistakes

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -6,7 +6,7 @@ import Course from "../models/course.js"
 export const clerkWebHooks=async(req,res)=>{
   try {
     const whook=new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-    await whook.verify(JSON.stringify(req.body),{
+    await whook.verify(req.body,{
         "svix-id":req.headers['svix-id'],
         "svix-timestamp":req.headers['svix-timestamp'],
         "svix-signature":req.headers['svix-signature']
